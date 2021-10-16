0d89d1f3894068525f9338120572b096ce79f86e
silvinaas <silvinaas4@gmail.com>
2021-10-16 01:07:43 +0000

    avance

---
+++ src/app.py
@@ -0,0 +1,20 @@
+import flask
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():   
+json_text = flask.jsonify(todos)
+return json_text
+
+  
+
+# These two lines should always be at the end of your app.py file.
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
