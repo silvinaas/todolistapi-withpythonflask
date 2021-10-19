da0c985a3022fe467271991bdf7341ae9cc80d68
silvinaas <silvinaas4@gmail.com>
2021-10-19 00:07:32 +0000

    ready

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 import flask
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
+from flask import jsonify
+import json
+
 app = Flask(__name__)
 
 todos = [
@@ -10,9 +13,24 @@ todos = [
 
 @app.route('/todos', methods=['GET'])
 def hello_world():   
-json_text = flask.jsonify(todos)
-return json_text
+  json_text = flask.jsonify(todos)
+  return json_text
 
+@app.route('/todos', methods=['POST'])
+def add_new_todo():   
+  request_body = request.data
+  decoded_object = json.loads(request_body)
+  todos[len(todos):] = [decoded_object]
+  todos_text = flask.jsonify(todos)
+  print("Incoming request with the following body", request_body)
+  return todos_text
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    remove_todo = todos.pop(position)
+    todos_text = flask.jsonify(todos)
+    print("This is the position to delete: ",position)
+    return todos_text
   
 
 # These two lines should always be at the end of your app.py file.
